a5eb76d99ca600439a90f1dceded9fecdce96b23
Maira Ussenbayeva <mairaussenbaeva@gmail.com>
2017-05-07 21:26:23 +0200

    Add Image

--- app/src/main/java/com/diarize/MainActivity.java
+++ app/src/main/java/com/diarize/MainActivity.java
@@ -1,7 +1,9 @@
 package com.diarize;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -12,7 +14,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.VideoView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -20,10 +24,14 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private static final int RES_IMAGE =1;
+    private static final int RES_VIDEO =1;
     TextView text;
     ImageButton photo;
     ImageButton video;
     ImageButton voice;
+    ImageView uploadImage;
+    VideoView uploadVideo;
     Button save;
 
     @Override
@@ -43,14 +51,19 @@ public class MainActivity extends AppCompatActivity {
         };
 
         text = (TextView) findViewById(R.id.addText);
+        uploadImage = (ImageView) findViewById(R.id.uploadImage);
         photo = (ImageButton) findViewById(R.id.addPhoto);
         photo.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                Intent galleryIntent  = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent,RES_IMAGE);
             }
         });
         video = (ImageButton) findViewById(R.id.addVideo);
         video.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                Intent galleryIntent  = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent,RES_VIDEO);
             }
         });
         voice = (ImageButton) findViewById(R.id.addVoice);
@@ -58,11 +71,42 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
             }
         });
+        uploadImage = (ImageView) findViewById(R.id.uploadImage);
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               /* Intent galleryIntent  = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent,RES_IMAGE);*/
+            }
+        });
+        uploadVideo = (VideoView) findViewById(R.id.uploadVideo);
+        uploadVideo.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               /* Intent galleryIntent  = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent,RES_IMAGE);*/
+            }
+        });
         save = (Button) findViewById(R.id.save);
         save.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
             }
         });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RES_IMAGE && resultCode == RESULT_OK && data != null)
+        {
+            Uri selectedImage = data.getData();
+            uploadImage.setImageURI(selectedImage);
+        }
+       else if (requestCode == RES_VIDEO && resultCode == RESULT_OK && data != null)
+        {
+            Uri selectedVideo = data.getData();
+            uploadVideo.setVideoURI(selectedVideo);
+            uploadVideo.requestFocus();
+            uploadVideo.start();
+        }
+
     }
 
     private void test() {
