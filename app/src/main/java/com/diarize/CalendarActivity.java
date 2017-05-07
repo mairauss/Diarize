92680e224d2e8bf0eeda5a29149ab5035e6d2acc
Yevhen Baidiuk <y.baidiuk@gmail.com>
2017-05-07 21:06:16 +0200

    add listVIew + inClickLisener

--- app/src/main/java/com/diarize/CalendarActivity.java
+++ app/src/main/java/com/diarize/CalendarActivity.java
@@ -1,37 +1,49 @@
 package com.diarize;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-import item_list.Item;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class CalendarActivity extends AppCompatActivity {
 
-    private List<Item> itemList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.calendar_layout_);
+        setContentView(R.layout.calendar_layout);
 
 
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getArray());
+        ListView listView = (ListView) findViewById(R.id.item_list);
+        listView.setBackgroundColor(Color.WHITE);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Intent toItemLayout = new Intent(v.getContext(), ItemView.class);
+                startActivity(toItemLayout);
+            }
+        });
     }
 
 
-    private void prepareitemData() {
-        Item Item1 = new Item(0, new Date(), "text  bla bla ", false, false, false);
-        itemList.add(Item1);
-
-        Item Item2 = new Item(0, new Date(), "text  bla bla ", false, false, false);
-        itemList.add(Item2);
-
-        Item Item3 = new Item(0, new Date(), "text  bla bla ", false, false, false);
-        itemList.add(Item3);
-
-        Item Item4 = new Item(0, new Date(), "text  bla bla ", false, false, false);
-        itemList.add(Item4);
+    private String[] getArray() {
+        String[] itemArr = new String[9];
+        itemArr[0] = "07.05.17  Dicko + Video";
+        itemArr[1] = "07.05.17  Dicko + Video";
+        itemArr[2] = "07.05.17  Dicko + Video";
+        itemArr[3] = "07.05.17  Dicko + Video";
+        itemArr[4] = "07.05.17  Dicko + Video";
+        itemArr[5] = "07.05.17  Dicko + Video";
+        itemArr[6] = "07.05.17  Dicko + Video";
+        itemArr[7] = "07.05.17  Dicko + Video";
+        itemArr[8] = "07.05.17  Dicko + Video";
+        return itemArr;
     }
 }
