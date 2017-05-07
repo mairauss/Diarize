e0df5ca355faac813494a5e9ccc5949eef113367
Yngve Skorge <a1506252@unet.univie.ac.at>
2017-05-07 13:06:23 +0200

    neues icon_launcher

--- app/src/main/java/com/diarize/ItemView.java
+++ app/src/main/java/com/diarize/ItemView.java
@@ -1,33 +1,38 @@
-//package com.diarize;
-//
-//import android.content.Intent;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.view.View;
-//import android.widget.Button;
-//
-//public class ItemView extends AppCompatActivity {
-//
-//
-//    Button shareButton;
-//    Intent shareIntent;
-//    String shareBody="Diarize is a really cool APP!! :)";
-//
-//
-//
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.item_view);
-//
-//        shareButton =(Button) findViewById(R.id.share_button);
-//        shareButton.setOnClickListener(new View.onClickListener(){
-//
-//            @Override
-//            public void onClickListener(View view){
-//                shareIntent=new Intent(Intent.ACTION_SEND);
-//            }
-//        });
-//    }
-//}
+package com.diarize;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ItemView extends AppCompatActivity {
+
+
+    Button shareButton;
+    Intent shareIntent;
+    String shareBody="Diarize is a really cool APP!! :)";
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.item_view);
+
+        shareButton =(Button) findViewById(R.id.share_button);
+
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+
+                shareIntent=new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("Text/plain");
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT,"My App");
+                shareIntent.putExtra(Intent.EXTRA_TEXT,shareBody);
+                startActivity(Intent.createChooser(shareIntent,"Share Via"));
+            }
+        });
+    }
+}
