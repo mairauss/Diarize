b4718cedcf89d2e84748dddf1fca799f2da96878
Yngve Skorge <a1506252@unet.univie.ac.at>
2017-05-12 22:43:57 +0200

    besseres design + profil2134

--- app/src/main/java/com/diarize/HelpActivity.java
+++ app/src/main/java/com/diarize/HelpActivity.java
@@ -1,13 +1,53 @@
 package com.diarize;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class HelpActivity extends AppCompatActivity {
+
+
+    EditText editText,editText3,editText2;
+
+    Button b_send;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+
+        editText=(EditText) findViewById(R.id.editText);
+        editText2=(EditText) findViewById(R.id.editText2);
+        editText3=(EditText) findViewById(R.id.editText3);
+
+        b_send= (Button) findViewById(R.id.b_send);
+
+        b_send.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view){
+                String to= editText.getText().toString();
+                String subject= editText3.getText().toString();
+                String message= editText2.getText().toString();
+
+                Intent intent= new Intent(Intent.ACTION_SEND);
+
+                intent.putExtra(Intent.EXTRA_EMAIL,new String[]{to});
+                intent.putExtra(Intent.EXTRA_SUBJECT,subject);
+                intent.putExtra(Intent.EXTRA_TEXT,message);
+
+                intent.setType("message/rfc822");
+
+                startActivity(Intent.createChooser(intent,"Select Email Application"));
+
+            }
+
+        } );
+
     }
 }
